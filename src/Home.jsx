4336125505cbb7f5d2977121118268d4feb68f86
Brahimrbahy <brahim.rbahy@gmail.com>
2025-07-02 00:51:39 +0100

    finH

--- src/Home.jsx
+++ src/Home.jsx
@@ -477,6 +477,79 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <div className="container mx-auto py-12">
+                <div className="px-16">
+                    <h2 className="text-2xl font-bold text-center uppercase mb-8">Our Blog</h2>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {blogData.map((post) => (
+                            <article key={post.id} className="bg-white max-w-[380px] mx-auto w-full roverflow-hidden">
+                                <div className="relative w-full h-0 pb-[72.3%] overflow-hidden ">
+                                    <img
+                                        src={post.image}
+                                        alt={post.title}
+                                        className="absolute top-0 left-0 w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-105"
+                                    />
+                                </div>
+
+                                <div className="pt-4 pb-6">
+                                    <a
+                                        href="#"
+                                        className=" font-normal text-[20px] text-gray-900 hover:text-[#e65540] transition-colors block pb-2"
+                                    >
+                                        {post.title}
+                                    </a>
+
+                                    <div className="text-[13px] text-gray-500 pb-3">
+                                        by <span className='font-medium'>{post.author}</span> on <span className='font-medium'>{post.date}</span>
+                                    </div>
+
+                                    <p className="text-[13px] text-gray-600 leading-relaxed">
+                                        {post.description}
+                                    </p>
+                                </div>
+                            </article>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="py-16">
+                <h2 className="text-[30px] font-bold text-black text-center mb-16">
+                    @ FOLLOW US ON INSTAGRAM
+                </h2>
+
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-col md:flex-row justify-between items-start text-center divide-y md:divide-y-0 md:divide-x divide-gray-200">
+                        <div className="w-full md:w-1/3 p-8">
+                            <h3 className="text-[18px] font-semibold text-[#555555] mb-2">
+                                Free Delivery Worldwide
+                            </h3>
+                            <p className="text-[13px] text-[#888888] italic">
+                                Mirum est notare quam littera gothica
+                            </p>
+                        </div>
+
+                        <div className="w-full md:w-1/3 p-8">
+                            <h3 className="text-[18px] font-semibold text-[#555555] mb-2">
+                                30 Days Return
+                            </h3>
+                            <p className="text-[13px] text-[#888888] italic">
+                                Simply return it within 30 days for an exchange.
+                            </p>
+                        </div>
+
+                        <div className="w-full md:w-1/3 p-8">
+                            <h3 className="text-[18px] font-semibold text-[#555555] mb-2">
+                                Store Opening
+                            </h3>
+                            <p className="text-[13px] text-[#888888] italic">
+                                Shop open from Monday to Sunday
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 };
